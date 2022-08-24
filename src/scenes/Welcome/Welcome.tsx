1ebe3a98a62aa4dfc390532c49492e35642ebe0e
Hiago Prates <hiagohps360@gmail.com>
2022-08-24 01:04:49 -0300

    feat: scenes

---
+++ src/scenes/Welcome/Welcome.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const Welcome: React.FC = () => {
+  return (
+    <View>
+      <Text>Scene Welcome</Text>
+    </View>
+  );
+};
+
+export default Welcome;
---
+++ src/scenes/Welcome/index.tsx
@@ -0,0 +1,11 @@
+import React, {FC} from 'react';
+
+import Welcome from './Welcome';
+
+type Props = {};
+
+const WelcomeContainer: FC<Props> = () => {
+  return <Welcome />;
+};
+
+export default WelcomeContainer;
